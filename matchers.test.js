7839abc6e4eea068e960974634115c6b0d56752a
steve.tsao <steve.tsao@hlwtech.com.tw>
2023-11-15 15:50:59 +0800

    test: update test files with new matchers
    - Remove the test for "toMatch 匹配器"
    - Add test for "toContain 匹配器"
    
    Signed-off-by: steve.tsao <steve.tsao@hlwtech.com.tw>

--- matchers.test.js
+++ matchers.test.js
@@ -56,7 +56,14 @@
 // });
 
 // String
-test("toMatch 匹配器", () => {
-  const str = "http://www.dell-lee.com";
-  expect(str).toMatch(/dell/);
+// test("toMatch 匹配器", () => {
+//   const str = "http://www.dell-lee.com";
+//   expect(str).toMatch(/dell/);
+// });
+
+// Array Set
+test("toContain 匹配器", () => {
+  const arr = ["dell", "lee", "imooc"];
+  const people = new Set(arr);
+  expect(people).toContain("dell");
 });
