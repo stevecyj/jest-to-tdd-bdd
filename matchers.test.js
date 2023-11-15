3bd93dbe9564aef1c9aa543a53dc0b6cc2719ed0
steve.tsao <steve.tsao@hlwtech.com.tw>
2023-11-15 15:22:30 +0800

    test: refactor test cases for matchers and test content equality
    - Remove the test case for "測試內容相等" and "not 匹配器"
    - Comment out multiple test cases for different matchers
    
    Signed-off-by: steve.tsao <steve.tsao@hlwtech.com.tw>

--- matchers.test.js
+++ matchers.test.js
@@ -3,7 +3,32 @@
 //   expect(a).toBe({ one: 1 });
 // });
 
-test("測試內容相等", () => {
-  const a = { one: 1 };
-  expect(a).toEqual({ one: 1 });
+// test("測試內容相等", () => {
+//   const a = { one: 1 };
+//   expect(a).toEqual({ one: 1 });
+// });
+
+// test("toBeUndefined 匹配器", () => {
+//   const a = undefined;
+//   expect(a).toBeUndefined();
+// });
+
+// test("toBeDefined 匹配器", () => {
+//   const a = null;
+//   expect(a).toBeDefined();
+// });
+
+// test("toBeTruthy 匹配器", () => {
+//   const a = 0;
+//   expect(a).toBeTruthy();
+// });
+
+// test("toBeFalsy 匹配器", () => {
+//   const a = 1;
+//   expect(a).toBeFalsy();
+// });
+
+test("not 匹配器", () => {
+  const a = 1;
+  expect(a).not.toBeFalsy();
 });
