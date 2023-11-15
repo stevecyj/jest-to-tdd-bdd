1de43dfb9746f0237c9c839cc5b92900abc51f30
steve.tsao <steve.tsao@hlwtech.com.tw>
2023-11-15 16:01:33 +0800

    test: remove unnecessary and add new test cases
    - Remove the test case for the `toContain` matcher
    - Add a new test case for the `throwNewErrorFunc` function
    
    Signed-off-by: steve.tsao <steve.tsao@hlwtech.com.tw>

--- matchers.test.js
+++ matchers.test.js
@@ -62,8 +62,16 @@
 // });
 
 // Array Set
-test("toContain 匹配器", () => {
-  const arr = ["dell", "lee", "imooc"];
-  const people = new Set(arr);
-  expect(people).toContain("dell");
+// test("toContain 匹配器", () => {
+//   const arr = ["dell", "lee", "imooc"];
+//   const people = new Set(arr);
+//   expect(people).toContain("dell");
+// });
+
+// Exception
+const throwNewErrorFunc = () => {
+  throw new Error("this is a new error");
+}
+test("throwNewErrorFunc should throw an error", () => {
+  expect(throwNewErrorFunc).toThrow("this is a new error");
 });
