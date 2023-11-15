00053b9f17948061dae808784baa5f354fccad5c
steve.tsao <steve.tsao@hlwtech.com.tw>
2023-11-15 18:29:46 +0800

    test: refactor test scripts and callbacks
    - Modify the test for `fetchData` to use a callback function and call `done()` to indicate completion
    - Change the `test` script in `package.json` to `jest --watch` instead of `jest --watchAll`
    
    Signed-off-by: steve.tsao <steve.tsao@hlwtech.com.tw>

--- fetchData.test.js
+++ fetchData.test.js
@@ -1,7 +1,8 @@
 import { fetchData } from "./fetchData";
 
-test("fetchData 返回結果為 { success: true}", () => {
+test("fetchData 返回結果為 { success: true}", (done) => {
   fetchData((data) => {
     expect(data).toEqual({ success: true });
+    done();
   });
 });
