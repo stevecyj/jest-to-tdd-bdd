445b85b6f89660edbd08672aad15f5e5ad4dea1c
steve.tsao <steve.tsao@hlwtech.com.tw>
2023-11-17 15:21:03 +0800

    feat: refactor fetchData to handle errors and improve data retrieval
    - Modify `fetchData` to return the axios request directly instead of calling a callback function
    - Add `fetchData404` function to handle 404 errors
    
    Signed-off-by: steve.tsao <steve.tsao@hlwtech.com.tw>

--- fetchData.js
+++ fetchData.js
@@ -1,7 +1,15 @@
 import axios from "axios";
 
+// export const fetchData = (fn) => {
+//   axios.get("http://www.dell-lee.com/react/api/demo.json").then((response) => {
+//     fn(response.data);
+//   });
+// };
+
 export const fetchData = (fn) => {
-  axios.get("http://www.dell-lee.com/react/api/demo.json").then((response) => {
-    fn(response.data);
-  });
+  return axios.get("http://www.dell-lee.com/react/api/demo.json")
+};
+
+export const fetchData404 = (fn) => {
+  return axios.get("http://www.dell-lee.com/react/api/demo1.json")
 };
--- fetchData.test.js
+++ fetchData.test.js
@@ -1,8 +1,22 @@
-import { fetchData } from "./fetchData";
+import { fetchData,fetchData404 } from "./fetchData";
 
-test("fetchData 返回結果為 { success: true}", (done) => {
-  fetchData((data) => {
-    expect(data).toEqual({ success: true });
-    done();
+// test("fetchData 返回結果為 { success: true}", (done) => {
+//   fetchData((data) => {
+//     expect(data).toEqual({ success: true });
+//     done();
+//   });
+// });
+
+// test("fetchData 返回結果為 { success: true}", () => {
+//   return fetchData().then((response) => {
+//     expect(response.data).toEqual({ success: true });
+//   });
+// });
+
+test("fetchData 返回結果為 404", () => {
+  expect.assertions(1);
+  return fetchData404().catch((e) => {
+    console.log(e.toString())
+    expect(e.toString().indexOf("404") > -1).toBe(true);
   });
 });
